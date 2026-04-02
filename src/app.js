b35047ab85d9157631b8f64ba5b9b0323d66757a
BorisEdison <borisedison@gmail.com>
2026-04-02 17:33:58 +0000

    moved health route inside routes folder

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express = require("express");
+const routes = require("./routes/index");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Server is running",
-  });
-});
+app.use("/", routes);
 
 module.exports = app;
